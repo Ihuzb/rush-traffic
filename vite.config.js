3e44cccbee564aba5b17bf5517498d14cbf5c14b
huzhibin@guigeinvest.com <guigeinvest.com>
2024-03-18 18:53:12 +0800

    #0318 打包提交

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,14 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import Components from 'unplugin-vue-components/vite';
 import {AntDesignVueResolver} from 'unplugin-vue-components/resolvers';
+
+let isPro = process.env.NODE_ENV === 'production';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-    // base: './',
+    base: '/rush-traffic/',
     build: {
-        outDir: 'docs',//想要把dist修改成什么名字在这边改
+        outDir: 'docs',
     },
     plugins: [vue(), Components({
         resolvers: [
