8f1d11be66deb63ff78cb2547b05ecc68d259157
huzhibin@guigeinvest.com <guigeinvest.com>
2024-03-18 18:14:43 +0800

    #0318 打包提交

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import Components from 'unplugin-vue-components/vite';
 import {AntDesignVueResolver} from 'unplugin-vue-components/resolvers';
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: './',
     plugins: [vue(), Components({
         resolvers: [
             AntDesignVueResolver({
