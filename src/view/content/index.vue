b36d78c10f2ec02801e7c3871f07d4ad1aac6a87
huzhibin@guigeinvest.com <guigeinvest.com>
2024-03-20 14:45:04 +0800

    #0320 右键拖拽

--- src/view/content/index.vue
+++ src/view/content/index.vue
@@ -3,7 +3,7 @@
     <div class="menu-class">
       <Menu/>
     </div>
-    <div class="content-class">
+    <div class="content-class" id="content" @contextmenu="e=>e.preventDefault()">
       <div id="scene">
         <Scene/>
       </div>
@@ -38,33 +38,36 @@ const setWheel = () => {
 }
 const setMouse = () => {
   let box = document.getElementById("scene");
+  let content = document.getElementById("content");
   // 鼠标在元素上按下开始拖拽
-  box.onmousedown = function () {
-    // 获取按下鼠标时 盒子与页面的距离
-    const originBoxX = box.offsetLeft;
-    const originBoxY = box.offsetTop;
-    // 获取按下鼠标时 鼠标与页面的距离
-    const mouseX = event.pageX;
-    const mouseY = event.pageY;
-    // 在页面上移动
-    window.onmousemove = function () {
-      // 鼠标滑动的距离 = 鼠标移动后的位置 - 按下鼠标时的位置
-      const distanceX = event.pageX - mouseX;
-      const distanceY = event.pageY - mouseY;
-      // 给元素重新赋值 上左定位的位置
-      box.style.left = originBoxX + distanceX + "px";
-      box.style.top = originBoxY + distanceY + "px";
-    };
-    // 鼠标松开取消事件
-    box.onmouseup = function () {
-      // 解绑在页面上滚动的事件
-      window.onmousemove = null;
-    };
+  box.onmousedown = function (event) {
+    if (event.button == 2) {
+      // 获取按下鼠标时 盒子与页面的距离
+      const originBoxX = box.offsetLeft;
+      const originBoxY = box.offsetTop;
+      // 获取按下鼠标时 鼠标与页面的距离
+      const mouseX = event.pageX;
+      const mouseY = event.pageY;
+      // 在页面上移动
+      content.onmousemove = function (event) {
+        // 鼠标滑动的距离 = 鼠标移动后的位置 - 按下鼠标时的位置
+        const distanceX = event.pageX - mouseX;
+        const distanceY = event.pageY - mouseY;
+        // 给元素重新赋值 上左定位的位置
+        box.style.left = originBoxX + distanceX + "px";
+        box.style.top = originBoxY + distanceY + "px";
+      };
+      // 鼠标松开取消事件
+      box.onmouseup = function () {
+        // 解绑在页面上滚动的事件
+        content.onmousemove = null;
+      };
+    }
   };
 }
 onMounted(() => {
   setWheel();
-  // setMouse();
+  setMouse();
 })
 </script>
 
@@ -83,6 +86,7 @@ onMounted(() => {
   }
 
   .content-class {
+    border: 1px solid gray;
     height: 100vh;
     width: calc(100vw - 400px);
     overflow: hidden;
